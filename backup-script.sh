7aa54f076a92e681661237d63d073e97195c0512
Pelle Hanses <pelle@masendata.com>
2024-02-15 23:16:36 +0100

    Ver 0.1

--- backup-script.sh
+++ backup-script.sh
@@ -3,15 +3,21 @@
 # Define variables
 SOURCE_DIR="/data"
 DESTINATION_DIR="/backup"
-TARGET_SERVER="remote_userpelle@target_server"
+TARGET_SERVER="remote_user@remote_server"
 SSH_KEY="~/.ssh/id_rsa.pub"
 
+# Find the latest backup directory on the target server
+latest_backup=$(ssh -i $SSH_KEY $TARGET_SERVER "ls -td $DESTINATION_DIR/*/ | head -n 1")
+
 # Rsync options
-RSYNC_OPTIONS="-av --delete --link-dest=../$(date -d '1 day ago' +%Y-%m-%d) --exclude='.git'"
+RSYNC_OPTIONS="-av --delete --link-dest=$latest_backup --exclude='.git'"
 
 # Perform backup
-rsync $RSYNC_OPTIONS -e "ssh -i $SSH_KEY" $SOURCE_DIR/ $TARGET_SERVER:$DESTINATION_DIR/$(date +%Y-%m-%d)/
+echo RSYNC
+echo rsync $RSYNC_OPTIONS -e "ssh -i $SSH_KEY" $SOURCE_DIR/ $TARGET_SERVER:$DESTINATION_DIR/$(date +%Y-%m-%d_%H-%M)/
+rsync $RSYNC_OPTIONS -e "ssh -i $SSH_KEY" $SOURCE_DIR/ $TARGET_SERVER:$DESTINATION_DIR/$(date +%Y-%m-%d_%H-%M)/
 
 # Clean up old backups
+echo Städar
 ssh -i $SSH_KEY $TARGET_SERVER "find $DESTINATION_DIR -maxdepth 1 -type d -mtime +7 -exec rm -rf {} \;"
 
